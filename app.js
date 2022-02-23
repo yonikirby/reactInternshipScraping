f69c517c4b1ea470a25951a33b7fe46457c597db
yonatanbenezra <yonatanbenezra1@gmail.com>
2022-02-23 13:03:27 +0200

    deploy

--- app.js
+++ app.js
@@ -312,8 +312,7 @@ app.get("/users/:number/studies", (req, res) => {
     if (err) {
       res.send(err);
     } else {
-      data.length > 0 ? res.json(data[0].studies) : res.send('אין מספר זה');
-      // res.json(data[0].studies);
+      res.json(data[0].studies);
     }
   });
 });
