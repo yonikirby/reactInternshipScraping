d139da6a5b161e54b9449401d7ce6781f96d083f
yonatanbenezra <yonatanbenezra1@gmail.com>
2022-02-28 22:46:00 +0200

    redeploy

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const fs = require('fs');
 const favicon = require('serve-favicon');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { google } = require('googleapis');
+const { object } = require('webidl-conversions')
+
+
+const CLIENT_ID = '512849584871-17jvkpfkt3ho4666e1vp9lg3cqraiml8.apps.googleusercontent.com'
+const CLIENT_SECRET = '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'
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+
+const REFRESH_TOKEN = '1//04xStBwrPjwgpCgYIARAAGAQSNwF-L9Ir0-iUDvVKFDuBKysSwCKh4FJZ19nCNg-wYIFIPRyMABAxzIPlh1Wfws_31gau53epxs0'
 
 app.use(cors())
 app.use(favicon(path.join(__dirname, "favicon.ico")));
@@ -439,4 +448,116 @@ app.delete('/milgot/delete/:id', (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+//google drive
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+)
+
+oauth2Client.setCredentials({
+  refresh_token: REFRESH_TOKEN
+})
+
+const drive = google.drive({
+  version: 'v3',
+  auth: oauth2Client
+})
+
+const filePath = path.join(__dirname, 'תיקוני לשון- סיכום (1).docx')
+
+
+async function uploadFile() {
+try {
+const response = await drive.files.create({
+requestBody: {
+  name: 'תיקוני לשון- סיכום (1).docx',
+  mimeType: 'application/vnd.google-apps.document'
+},
+media: {
+  mimeType: 'application/vnd.google-apps.document',
+  body: fs.createReadStream(filePath)
+
+  }
+});
+
+} catch (err) {
+  console.error(err.message)
+}
+}
+
+app.get('/getQandA/:id', async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+      keyFile: 'credentials.json',
+      scopes: 'https://www.googleapis.com/auth/spreadsheets'
+  })
+
+  //create client instance
+  const client = await auth.getClient()
+
+  //instance of google sheets api
+  const googlesheets = google.sheets({
+      version: 'v4',
+      auth: client
+  }) 
+
+  const id = req.params.id
+
+  //get meta data about spreasheet
+  const data = await googlesheets.spreadsheets.get({
+      auth,
+      spreadsheetId: id
+  })
+
+  //read rows from spreadsheet
+  const getRows = await googlesheets.spreadsheets.values.get({
+      auth, 
+      spreadsheetId: id,
+      range: data.data.sheets[0].properties.title
+  })
+
+  const values = getRows.data.values
+  delete values[0]
+  let questions = []
+  if(values.length > 0){
+      let i = 1;
+      let questionsArray = []
+      values.forEach((data, index) => {
+          questions.push({
+              question_id: index,
+              question: data[0],
+              answers: [
+                  {
+                      answer_id: 1,
+                      text: data[1],
+                      correct: true
+                  },
+                  {
+                      answer_id: 2, 
+                      text: data[2],
+                      correct: false
+                  },
+                  {
+                      answer_id: 3,
+                      text: data[3],
+                      correct: false
+                  },
+                  {
+                      answer_id: 4,
+                      text: data[4],
+                      correct: false
+                  }
+              ]
+          })
+      })
+
+  }
+  console.log(questions)
+
+  res.send(questions)
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
